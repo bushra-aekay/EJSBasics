234bd5d70c5363f3cf7f6980ac6903ebdd8924be
Bushra Abdul Khader <92054620+bushra-aekay@users.noreply.github.com>
2024-10-28 04:09:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+
+// var date = new Date(); //gets current date and storing in var date.
+var date = new Date("June 24, 2023 11:13:00");
+var day = date.getDay(); //method to get the day of the date returned above and stores in var called day.
+console.log(day);
+
+let type = 'a weekday';
+let adv = 'its time to chill and procastinate';
+
+if (day === 0 || day ===6){
+    type = 'the weekend';
+    adv = 'time to lock in bestie'
+}
+
+app.get("/", (req,res) =>{
+    res.render(__dirname + "/views/index.ejs",{
+        dayType: type,
+        advice: adv,
+    });
+});
+
+app.listen( port, ()=>{
+    console.log(`Listening for requests at ${port} `);
+});
